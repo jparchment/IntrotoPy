49c919b1a964fc369f3b74521c2dde155b61cd49
jparchment <71052504+jparchment@users.noreply.github.com>
2020-10-04 13:42:29 -0400

    Update __init__.py

--- Nums_Variables/__init__.py
+++ Nums_Variables/__init__.py
@@ -8,9 +8,9 @@ def exists():
 @check50.check(exists)
 def compiles():
     """no syntax errors in num_variables.py."""
-    check50.run("python -m py_compile mario.py").exit(0)
+    check50.run("python -m py_compile num_variables.py").exit(0)
     
 @check50.check(compiles)
-def prints_four_question_marks():
-    """prints four question marks"""
-    check50.run("python mario.py").stdout("\?\?\?\?", "????").exit(0)    
+def prints_variables():
+    """prints variables"""
+    check50.run("python nums_variables.py").stdout("print(years)", "print(years_str)", print("years"), "10", "10", "20").exit(0)    
