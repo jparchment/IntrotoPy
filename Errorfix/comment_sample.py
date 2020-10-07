7f6eab36c8478d80ca3b40205f04fd5cf04168bf
jparchment <71052504+jparchment@users.noreply.github.com>
2020-10-07 08:47:27 -0400

    Create comment_sample.py

---
+++ Errorfix/comment_sample.py
@@ -0,0 +1,27 @@
+# prints the string "Learning to code is hard" to the screen
+print("Learning to code is hard")
+ 
+# prints "but with the right motivation, I know I can do it." to the screen
+print("but with the right motivation, I know I can do it.")
+ 
+# creates a variable 'u' and assigns it a value of 12.
+# DO NOT say 'this makes u = 12'.
+u = 12
+ 
+# changes the value of 'u' to 17 (it's not 12 anymore).
+u = 17
+ 
+# creates a variable x and assigns it a value of 2.
+x = 2
+ 
+# creates a variable z and sets it equal to x + u.
+# the value of z is saved as 19.
+z = x + u
+ 
+# prints the value of 'z' to the screen
+print(z)
+ 
+# prints the value of 'u' to the screen
+print(u)
+ 
+
