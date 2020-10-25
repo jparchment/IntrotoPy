7019b14a22399ae7c7b33e6313a4c38f56b68437
jparchment <71052504+jparchment@users.noreply.github.com>
2020-10-25 18:22:14 -0400

    Create input_ex2.py

---
+++ InputExamples/input_ex2.py
@@ -0,0 +1,17 @@
+# asks the user for two numbers
+num1 = int(input("Number 1: "))
+num2 = int(input("Number 2: "))
+
+# performs mathematical operations with num1 and num2
+# and prints the result
+
+print("Sum: ", num1 + num2)
+print("Difference: ", num 1 - num2)
+print("Product: " # input your code here)
+
+# write your own code to calculate the quotient
+
+# write your own code to calculate integer division
+
+# write your own code to find the remainder
+
