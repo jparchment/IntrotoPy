b19eb9166485ffcfe84e152bb20c10ca819d3b3f
jparchment <71052504+jparchment@users.noreply.github.com>
2020-10-25 18:57:53 -0400

    Update input_ex2.py

--- InputExamples/input_ex2.py
+++ InputExamples/input_ex2.py
@@ -9,9 +9,9 @@ print("Sum: ", num1 + num2)
 print("Difference: ", num 1 - num2)
 print("Product: " # input your code here)
 
-# write your own code to calculate the quotient
+# write your own code to calculate and print the quotient (match the format above)
 
-# write your own code to calculate integer division
+# write your own code to calculate and print the result of integer division (match the format)
 
-# write your own code to find the remainder
+# write your own code to find and print the remainder (match the format)
 
