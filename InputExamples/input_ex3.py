608a915b849978b6dd15ee8fc316aaf805a61b00
jparchment <71052504+jparchment@users.noreply.github.com>
2020-10-25 19:14:55 -0400

    Update input_ex3.py

--- InputExamples/input_ex3.py
+++ InputExamples/input_ex3.py
@@ -1,9 +1,7 @@
 # asks the user to input a number as an integer, specifically
-number1 = int(input("Please enter a number as an integer: ")
+number1 = int(input("Please enter a number as an integer: "))
 
 # outputs the next three consecutive numbers
-a = number1 + 1
-b = number1 + 2
-c = number1 + 3
+print(number1 + 1, number1 + 2. number1 + 3)
              
-print(a, b, c)
+
