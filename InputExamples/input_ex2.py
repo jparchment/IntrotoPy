77de10199a15747810f291ec7b60e1f15103a2b8
jparchment <71052504+jparchment@users.noreply.github.com>
2020-10-25 19:01:02 -0400

    Update input_ex2.py

--- InputExamples/input_ex2.py
+++ InputExamples/input_ex2.py
@@ -6,7 +6,7 @@ num2 = int(input("Number 2: "))
 # and prints the result
 
 print("Sum: ", num1 + num2)
-print("Difference: ", num 1 - num2)
+print("Difference: ", num1 - num2)
 print("Product: " # input your code here)
 
 # write your own code to calculate and print the quotient (match the format above)
