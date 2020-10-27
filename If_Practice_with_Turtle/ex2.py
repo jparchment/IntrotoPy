244a1a5e9385bcd8a5394656215cb81a85cd9e46
jparchment <71052504+jparchment@users.noreply.github.com>
2020-10-27 18:45:09 -0400

    Create ex2.py

---
+++ If_Practice_with_Turtle/ex2.py
@@ -0,0 +1,7 @@
+x = float(input('Enter a number: ')
+if x != 3:
+  z = input("What is your name? ")
+  y = z + 3
+  print(y)
+print('Now the programs returns to the \'normal\' code')
+print('What happens if the user inputs the number 3?')
