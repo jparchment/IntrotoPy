6f935aa017bd429b7fd2cdfe55d6bad8d9792112
jparchment <71052504+jparchment@users.noreply.github.com>
2020-10-27 19:02:38 -0400

    Update ex3.py

--- If_Practice_with_Turtle/ex3.py
+++ If_Practice_with_Turtle/ex3.py
@@ -14,14 +14,14 @@ if x > 0:
   turtle.dot(100, 'cyan')
   
 # ----------------------
-y = int(input('Please enter an integer greater than 0: ')
+y = int(input('Please enter an integer greater than 0: '))
 if y <= 0:
-  y = int(input('Invalid input. You must enter an integer greater than 0. Try again >> ')
+  y = int(input('Invalid input. You must enter an integer greater than 0. Try again >> '))
 
 if y > 10:
   turtle.dot(50, 'blue')
   turtle.dot(30, 'dark green')
   turtle.dot(10, 'magenta')
 
-print('Nothing more to do)
+print('Nothing more to do')
   
