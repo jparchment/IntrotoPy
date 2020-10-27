acc60842b0f3bbace2f7c0899da5b22395dc06e4
jparchment <71052504+jparchment@users.noreply.github.com>
2020-10-27 19:11:44 -0400

    Update ex1.py

--- If_Practice_with_Turtle/ex1.py
+++ If_Practice_with_Turtle/ex1.py
@@ -1,4 +1,10 @@
+# Define the variables x and y. In order to use a variable
+# within our if block, we must first define it
+# before the if block
 x = int(input('Enter a number: '))
+y = 0
 if x <= 4:
     y = x ** 2
-print('The value of x squared is ', y)
+    print('The value of x squared is ', y)
+print('The value of x is', x, 'which is not less than or equal to 4.')
+print('Therefore, no code within the if block is executed.')
