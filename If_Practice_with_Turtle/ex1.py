18dc311c3d4a905a46f9b878acac37401c251880
jparchment <71052504+jparchment@users.noreply.github.com>
2020-10-27 19:34:53 -0400

    Update ex1.py

--- If_Practice_with_Turtle/ex1.py
+++ If_Practice_with_Turtle/ex1.py
@@ -1,10 +1,10 @@
 # Define the variables x and y. In order to use a variable
-# within our if block, we must first define it
-# before the if block
+# within our if block, we must first define it before the if block
 x = int(input('Enter a number: '))
 y = 0
 
-# code which will be executed if the given condition is True.
+# The code in this if block will be executed if the given condition is True.
+# If the condition is False, this code will not be executed.
 if x <= 4:
     y = x ** 2
     print('The value of x squared is ', y)
