56709ceb70cf042cc02fbf848b01faa55d0863d9
jparchment <71052504+jparchment@users.noreply.github.com>
2020-10-27 19:55:45 -0400

    Update ex2.py

--- If_Practice_with_Turtle/ex2.py
+++ If_Practice_with_Turtle/ex2.py
@@ -1,7 +1,12 @@
+# creates the variable x which takes input as an integer OR a float
 x = float(input('Enter a number: '))
+
+# If block will be executed if the condition is True. 
 if x != 3:
   z = input("What is your name? ")
-  y = z + 3
-  print(y)
+  y = 'Hi'
+  print(y, z, '\U00002757\U0001F600') # unicode to print emojis
+
+# This part of the program will always be executed because it is outdented.  
 print('Now the programs returns to the \'normal\' code')
-print('What happens if the user inputs the number 3?')
+print('What happens if the user inputs the number 3.0?')
