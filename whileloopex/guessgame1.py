6a7c3a057961a80b82baf834059cb26633060f61
jparchment <71052504+jparchment@users.noreply.github.com>
2020-12-17 01:00:43 -0500

    Update guessgame1.py

--- whileloopex/guessgame1.py
+++ whileloopex/guessgame1.py
@@ -2,24 +2,24 @@ import random
 
 secret_number =  random.randint(1,10)
 
-my_guess = int(input("You've got one chance. Guess the secret number between 1 and 10 >>> "))
+my_guess = int(input("\nYou've got one chance. Guess the secret number between 1 and 10 >>> "))
 
-count = 0
+count = 1
 
 while my_guess != secret_number:   
     
-  my_guess = int(input("You've got another chance. Guess the secret number between 1 and 10 >>> "))
+  my_guess = int(input("\nYou've got another chance. Guess the secret number between 1 and 10 >>> "))
 
   count += 1
 
   if my_guess == secret_number:
-    print("You got it!")
+    print("\nYou got it!")
         
   elif my_guess > secret_number:
-    print('Your guess was too high!')
+    print('\nYour guess was too high!')
 
   else:
-    print('Your guess was too low!')
+    print('\nYour guess was too low!')
 
-print("Woo hoo! The secret number is:", secret_number, end = '. ')
+print("\nWoo hoo! The secret number is:", secret_number, end = '. ')
 print("You guessed", count,"times.")
