8c583126024e3d49edd5751c528aa629d3d60705
jparchment <71052504+jparchment@users.noreply.github.com>
2021-01-17 16:15:45 -0500

    Update ex5.py

--- For_Loops_Turtle/ex5.py
+++ For_Loops_Turtle/ex5.py
@@ -1,9 +1,22 @@
+# Draws a spiral pattern using a for loop
+
+# imports turtle module
 import turtle
 
+# sets up turtle
+turtle.showturtle()
+turtle.shape('arrow')
 
+# try changing the pensize number 
+turtle.pensize(4)
+
+# sets the pen color to blue and the background to purple
+turtle.color('blue')
+turtle.bgcolor('purple')
+
+# draws spiral
 length = 10
 angle = 90
-turtle.shape("turtle")
 turtle.forward(length+length)
 turtle.right(angle)
 length = length + 10
