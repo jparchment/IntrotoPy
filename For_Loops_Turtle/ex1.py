ba487253da6c8f106d266cd625d8ace31581fa0c
jparchment <71052504+jparchment@users.noreply.github.com>
2021-01-17 14:19:18 -0500

    Update ex1.py

--- For_Loops_Turtle/ex1.py
+++ For_Loops_Turtle/ex1.py
@@ -1,1 +1,23 @@
+# draws a single square using turtle graphics
 
+# imports turtle module
+import turtle
+
+# sets up turtle
+turtle.showturtle()
+turtle.shape('turtle')
+turtle.color('purple')
+turtle.start_fill()
+
+# draws square
+turtle.forward(100)
+turtle.right(90)
+
+turtle.forward(100)
+turtle.right(90)
+
+turtle.forward(100)
+turtle.right(90)
+
+turtle.forward(100)
+turtle.right(90)
