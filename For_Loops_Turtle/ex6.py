ca73203adf266648fb3b2b4a849f4b84cfbdb058
jparchment <71052504+jparchment@users.noreply.github.com>
2021-01-17 16:31:08 -0500

    Update ex6.py

--- For_Loops_Turtle/ex6.py
+++ For_Loops_Turtle/ex6.py
@@ -1,10 +1,32 @@
-# set up your turtle
+# creates spiral using a nested for loop, changes color of each spiral
 
-for x in range(5):
+# import turtle
+import turtle
+
+# set up your turtle
+turtle.showturtle()
+turtle.shape('arrow')
+
+# try changing the pensize number 
+turtle.pensize(4)
+
+# sets the background to purple
+turtle.bgcolor('purple')
+
+# change the length and angle to see how spiral changes
+length = 10
+angle = 90
+
+# creates a spiral 3 times
+for x in range(3):
   color = input("What color would you like? ")
-  
-  # for loop to recreate spiral
+  # changes turtle color (based on user input) after completion of each spiral
+  turtle.color(color)
+
+  # for loop to create one spiral
+  # COMPLETE THE FOR LOOP
+  # CHANGE NUMBER FOR THE RANGE TO SEE HOW SPIRAL CHANGES
   for x in range(?):
-    length = 10
-    angle = 90
-    turtle.
+      
+
+input("Press 'Enter' to clear image.")
