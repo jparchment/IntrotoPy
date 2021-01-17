967334779a6b84c5e592dfdbaf798b6a85d07ab7
jparchment <71052504+jparchment@users.noreply.github.com>
2021-01-17 15:14:17 -0500

    Create ex6.py

---
+++ For_Loops_Turtle/ex6.py
@@ -0,0 +1,10 @@
+# set up your turtle
+
+for x in range(5):
+  color = input("What color would you like? ")
+  
+  # for loop to recreate spiral
+  for x in range(?):
+    length = 10
+    angle = 90
+    turtle.
