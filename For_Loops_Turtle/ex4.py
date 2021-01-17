fc9a6227b85a345ffcee1b214bc1473e79637001
jparchment <71052504+jparchment@users.noreply.github.com>
2021-01-17 15:05:21 -0500

    Update ex4.py

--- For_Loops_Turtle/ex4.py
+++ For_Loops_Turtle/ex4.py
@@ -1,1 +1,25 @@
+# Draws a zigzag pattern using a for loop
 
+# imports turtle module
+import turtle
+
+# sets up turtle
+turtle.showturtle()
+turtle.shape('circle')
+
+# try changing the pensize number 
+turtle.pensize(8)
+
+# sets the fill color to purple and the border to green
+turtle.color('orange')
+turtle.bg('red')
+
+# draws zigzag usng a for loop
+
+# CREATE FOR LOOP TO REPRODUCE ZIGZAG
+
+for x in range(?):
+  turtle.forward(100)
+  turtle.right(90)
+
+input('Hit \'Enter\' to clear image')
