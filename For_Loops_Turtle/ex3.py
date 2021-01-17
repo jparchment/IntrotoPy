d19bd692bcc4c1e662a724f5d33568311edfc5b6
jparchment <71052504+jparchment@users.noreply.github.com>
2021-01-17 15:00:30 -0500

    Update ex3.py

--- For_Loops_Turtle/ex3.py
+++ For_Loops_Turtle/ex3.py
@@ -1,2 +1,38 @@
 # creates a zigzag pattern
 
+# imports turtle module
+import turtle
+
+# sets up turtle
+turtle.showturtle()
+turtle.shape('circle')
+
+# try changing the pensize number 
+turtle.pensize(8)
+
+# sets the color to orange and the background to red
+turtle.color('orange')
+turle.bgcolor('red')
+
+# draws zigzag
+
+length = 25
+angle = 45
+turtle.showturtle()
+turtle.shape("turtle")
+turtle.forward(length)
+turtle.right(angle)
+turtle.forward(length)
+turtle.left(angle)
+turtle.forward(length)
+turtle.right(angle)
+turtle.forward(length)
+turtle.left(angle)
+turtle.forward(length)
+turtle.right(angle)
+turtle.forward(length)
+turtle.left(angle)
+turtle.forward(length)
+turtle.right(angle)
+turtle.forward(length)
+
