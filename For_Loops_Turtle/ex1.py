0d0eda3a3767117e738ffabab40743e9cce6f7a3
jparchment <71052504+jparchment@users.noreply.github.com>
2021-01-17 14:31:25 -0500

    Update ex1.py

--- For_Loops_Turtle/ex1.py
+++ For_Loops_Turtle/ex1.py
@@ -6,8 +6,13 @@ import turtle
 # sets up turtle
 turtle.showturtle()
 turtle.shape('turtle')
-turtle.color('purple')
-turtle.start_fill()
+
+# try changing the pensize number 
+turtle.pensize(8)
+
+# sets the fill color to purple and the border to green
+turtle.color('green', 'purple')
+turtle.begin_fill()
 
 # draws square
 turtle.forward(100)
@@ -22,5 +27,6 @@ turtle.right(90)
 turtle.forward(100)
 turtle.right(90)
 
+turtle.end_fill()
 input('This program can be written using a for loop. Check out ex2.py. Press Enter.')
 
