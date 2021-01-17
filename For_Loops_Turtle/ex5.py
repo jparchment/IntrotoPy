356b547e1da773a84176908d530141dd92f8e873
jparchment <71052504+jparchment@users.noreply.github.com>
2021-01-17 15:11:44 -0500

    Create ex5.py

---
+++ For_Loops_Turtle/ex5.py
@@ -0,0 +1,48 @@
+import turtle
+
+
+length = 10
+angle = 90
+turtle.shape("turtle")
+turtle.forward(length+length)
+turtle.right(angle)
+length = length + 10
+turtle.forward(length+length)
+turtle.right(angle)
+length = length + 10
+turtle.forward(length+length)
+turtle.right(angle)
+length = length + 10
+turtle.forward(length+length)
+turtle.right(angle)
+length = length + 10
+turtle.forward(length+length)
+turtle.right(angle)
+length = length + 10
+turtle.forward(length+length)
+turtle.right(angle)
+length = length + 10
+turtle.forward(length+length)
+turtle.right(angle)
+length = length + 10
+turtle.forward(length+length)
+turtle.right(angle)
+length = length + 10
+turtle.forward(length+length)
+turtle.right(angle)
+length = length + 10
+turtle.forward(length+length)
+turtle.right(angle)
+length = length + 10
+turtle.forward(length+length)
+turtle.right(angle)
+length = length + 10
+turtle.forward(length+length)
+turtle.right(angle)
+length = length + 10
+turtle.forward(length+length)
+turtle.right(angle)
+length = length + 10
+turtle.forward(length+length)
+turtle.right(angle)
+length = length + 10
