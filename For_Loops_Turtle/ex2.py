d145beb76a702a4a4d036f1cedb7c19ae4ade503
jparchment <71052504+jparchment@users.noreply.github.com>
2021-01-17 14:35:31 -0500

    Create ex2.py

---
+++ For_Loops_Turtle/ex2.py
@@ -0,0 +1,28 @@
+# Draws a square using a for loop
+
+# imports turtle module
+import turtle
+
+# sets up turtle
+turtle.showturtle()
+turtle.shape('turtle')
+
+# try changing the pensize number 
+turtle.pensize(8)
+
+# sets the fill color to purple and the border to green
+turtle.color('green', 'purple')
+turtle.begin_fill()
+
+# draws square usng a for loop
+# WHAT NUMBER SHOULD BE INPUT FOR THE RANGE TO DRAW A SQUARE?
+# CORRECT ANY SYNTAX ERRORS BELOW
+
+for x in range(?)
+turtle.forward(100)
+turtle.right(90)
+
+# not part of the loop. Why not?
+turtle.end_fill
+
+input('Hit /'Enter/' to clear image')
